f6b0a75ddad51d55d3fceca2af3523f9fbb8b38f
Jenn Leung <jenn.leung@samsara.com>
2017-10-09 19:09:15 +1000

    removing coffee for now

--- coffee/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Navigation from './Navigation';
-import './index.css';
-
-ReactDOM.render(
-  <Navigation />,
-  document.getElementById('root')
-);
